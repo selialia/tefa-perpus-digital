3d30b09bdba6d637a3e478dfc3c33b1797cb35fd
selialiaputri <142282146+selialia@users.noreply.github.com>
2024-02-26 16:07:10 +0700

    Add files via upload

---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <input type="search" class="form-control from-control-lg rounded-5" placeholder="Filter...">
+        </div>
+        <div clas="my-3 text-muted">menampilkan 1 dari 1</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>No</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>Seli</td>
+              <td>Siswa</td>
+              <td>26 februari 2024, 23.31.00</td>
+              <td>Baca</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
